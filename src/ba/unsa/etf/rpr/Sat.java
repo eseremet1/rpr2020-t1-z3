f062ee611af9dbcb3ef9912630e2d16e365ebdc8
eseremet1 <eseremet1@etf.unsa.ba>
2020-10-30 18:35:40 +0100

    Uradjen zadatak

---
+++ src/ba/unsa/etf/rpr/Sat.java
@@ -0,0 +1,81 @@
+package ba.unsa.etf.rpr;
+
+public class Sat {
+    public int getSati() {
+        return sati;
+    }
+
+    public void setSati(int sati) {
+        this.sati = sati;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getSekunde() {
+        return sekunde;
+    }
+
+    public void setSekunde(int sekunde) {
+        this.sekunde = sekunde;
+    }
+
+    int sati,minute,sekunde;
+    public Sat(int sati, int minute, int sekunde) {
+        postavi(sati,minute,sekunde);
+    }
+
+    public void postavi(int sati, int minute, int sekunde) {
+        this.sati=sati;
+        this.minute=minute;
+        this.sekunde=sekunde;
+    }
+
+    public void ispisi() {
+        System.out.println(sati + ":" + minute + ":" + sekunde);
+    }
+
+    public void sljedeci() {
+        sekunde = sekunde + 1;
+        if (sekunde == 60) {
+            sekunde = 0;
+            minute = minute + 1;
+        }
+        if (minute == 60) {
+            minute = 0;
+            sati = sati + 1;
+        }
+        if (sati == 24)
+            sati = 0;
+    }
+
+    public void prethodni() {
+        sekunde = sekunde - 1;
+        if (sekunde == -1) {
+            sekunde = 59;
+            minute = minute - 1;
+        }
+        if (minute == -1) {
+            minute = 59;
+            sati = sati - 1;
+        }
+        if (sati == -1)
+            sati = 23;
+    }
+
+    public void pomjeriZa(int pomak) {
+        if (pomak > 0) {
+            for(int i = 0; i < pomak; i++) {
+                sljedeci();
+            }
+        }
+		else
+		    for (int i = 0; i < -pomak; i++)
+		        prethodni();
+    }
+}
